5bd59a6676af2ca3bcaca9f34101c0c0f5b84a6e
ELEzequielLambolla <130775757+ELambollaEzequiel@users.noreply.github.com>
2023-10-02 14:34:12 +0000

    masInfo renderizado

--- assets/main.js
+++ assets/main.js
@@ -7,6 +7,9 @@ const cartContainerListContainer = document.querySelector(
 );
 const cartTotal = document.querySelector(".cart-total");
 const cantProdEnCarrro = document.querySelector(".circle-mensaje");
+//mas info container
+const masInfoContainer = document.querySelector('.cards-masInfo')
+const masInfoBody = document.querySelector('.masInfo-body')
 
 console.log(cardsContainer);
 
@@ -47,7 +50,7 @@ const renderCarProduct = (arrProductos) => {
 console.log(cartContainer);
 console.log(cartBtn);
 // cambiar el estado del cart container apretando el boton
-const cartVisibility = () => {
+const containersVisibility = () => {
   cartContainer.classList.toggle("cart-hidden");
   cartContainer.classList.add("cart-show");
 };
@@ -128,12 +131,113 @@ const total = () => {
   cartTotal.innerText = `Total : $ ${totalCarrito}`;
 };
 
+//MAS INFO LOGICA Y RENDERIZADO
+
+//mostrar y ocultar mas info
+//mostrar mas info desde el card container
+const btnMasInfo = (e) => {
+  if (!e.target.classList.contains("card-masInfo")) return
+  masInfoContainer.classList.toggle("masInfo-hiden");
+  masInfoContainer.classList.add("masInfo-show");
+
+}
+
+//cerrar mas info desde close header
+const masInfoClose = (e) => {
+  if (!e.target.classList.contains("closeHeader")) return
+  masInfoContainer.classList.toggle("masInfo-show");
+  masInfoContainer.classList.add("masInfo-hiden");
+}
+//crear el inner HTML de mas info
+const masInfoInyection = (producto) => {
+  return `
+        <div class="imasges-container">
+          <div class="img-list">
+            <ul class="list-img-div">
+              <li class="box-img">
+                <img
+                  src=" ${producto.imgUrl} "
+                  class="image-masInfo-little"
+                  alt=""
+                  srcset=""
+                />
+              </li>
+              <li class="box-img">
+                <img
+                  src="${producto.imgUrl}"
+                  class="image-masInfo-little"
+                  alt=""
+                  srcset=""
+                />
+              </li>
+              <li class="box-img">
+                <img
+                  src="${producto.imgUrl}"
+                  class="image-masInfo-little"
+                  alt=""
+                  srcset=""
+                />
+              </li>
+              <li class="box-img">
+                <img
+                  src="${producto.imgUrl}"
+                  class="image-masInfo-little"
+                  alt=""
+                  srcset=""
+                />
+              </li>
+            </ul>
+          </div>
+          <div class="img-princ">
+            <img
+              src="${producto.imgUrl}"
+              alt=""
+              class="image-masInfo-princ"
+              srcset=""
+            />
+          </div>
+        </div>
+        <div class="description-container">
+          <div class="description-header">
+            <h4>${producto.nombre}</h4>
+          </div>
+          <div class="description-body">
+            <p>
+            ${producto.descripcion}
+            </p>
+            <p>Precio: $ ${producto.precio} </p>
+            <p>Cantidad : <span class="add-card-nfo card-info-addd">⬅</span> 0 <span class="add-card-nfo card-info-rest">➡</span></p>
+          </div>
+          <div class="description-footer">
+            <button class="btn-masInfo btn-comprar">Add to cart</button>
+            <button class="btn-masInfo btn-descripcion-comprar">Comprar</button>
+          </div>
+        </div>
+  `
+}
+
+//buscar el id del prod seleccionado y renderizarlo
+const masInfoId = (e) => {
+  if (!e.target.classList.contains("card-masInfo")) return;
+  const filterId = Number(e.target.dataset.id);
+  const prodId = productos.find((prod) => prod.id === filterId);
+  console.table(prodId);
+  masInfoBody.innerHTML = "";
+  masInfoBody.innerHTML = masInfoInyection(prodId)
+};
+
+
 const init = () => {
   renderCarProduct(productos);
   renderCartProduct(carrito);
   cantProductos();
   total();
-  cartBtn.addEventListener("click", cartVisibility);
+  cartBtn.addEventListener("click", containersVisibility);
   cardsContainer.addEventListener("click", encontrarId);
+  cardsContainer.addEventListener('click', btnMasInfo)
+  cardsContainer.addEventListener('click', masInfoId)
+  masInfoContainer.addEventListener('click', masInfoClose)
+  cardsContainer.addEventListener('click', masInfoId)
+
 };
 init();
